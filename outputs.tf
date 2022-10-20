b7409ddce89682b39d074ddf330d85d306e7b278
Alexander Kharkevich <alexander_kharkevich@epam.com>
2022-10-20 17:54:53 -0400

    fix: fix primary key output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "primary_key" {
-  value       = data.azurerm_function_app_host_keys.this.primary_key
+  value       = var.use_private_net ? data.azurerm_function_app_host_keys.this_vnet[0].primary_key : data.azurerm_function_app_host_keys.this[0].primary_key
   description = "Function app primary key"
 }
 
@@ -11,4 +11,9 @@ output "id" {
 output "identity" {
   value       = azurerm_linux_function_app.this.identity.*
   description = "Function app Managed Identity"
+}
+
+output "outbound_ip_address_list" {
+  value       = azurerm_linux_function_app.this.outbound_ip_address_list
+  description = "Function app outbound IP address list"
 }
